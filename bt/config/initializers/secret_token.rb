6a98e345511ef8a8979509f0066cfbcf7d482f41
Rick Whalley <rickwhalley@live.com>
2013-09-25 22:11:44 +0100

    initalize rails webapp

---
+++ bt/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bt::Application.config.secret_key_base = 'fba510992038c8f2fceb38fff46fb6f7ce8401db86b100734e23fc3957df350cf2e5235b9075117ec71d51009f58494526f54ba210d463d4d916bd6734cdfa16'
